d33c06c6fccb2f3c8f847f57753f8740901192ad
Rafael Felix <rafelix@berkeley.edu>
2018-04-19 01:44:26 -0700

    Added examples

--- goldbach.py
+++ goldbach.py
@@ -8,10 +8,8 @@ def is_prime(num):
 
 	>>> is_prime(1)
 	False
-
 	>>> is_prime(21)
 	False
-
 	>>> is_prime(47)
 	True
 	"""
@@ -30,12 +28,11 @@ def sieve(num):
 	"""Computes all primes less than or equal to a number,
 	following the Sieve of Erathostenes.
 
+
 	>>> sieve(17)
 	{2, 3, 5, 7, 11, 13, 17}
-
 	>>> sieve(50)
 	{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47}
-
 	>>> sieve(8)
 	{2, 3, 5, 7}
 	"""
@@ -55,6 +52,7 @@ def goldbach(num):
 	"""Returns a group of 3 primes that sum up to a number,
 	according to Goldbach's Weak Conjecture.
     
+    
     >>> goldbach(8)
     [3, 3, 2]
     >>> goldbach(754)
@@ -62,7 +60,7 @@ def goldbach(num):
     >>> goldbach(70000)
     [2, 69991, 7]
     """
-    
+
 	sieve_ = sorted(sieve(num), reverse=True)
 	sumands = []
 	for a, b in itertools.combinations(sieve_, 2):
