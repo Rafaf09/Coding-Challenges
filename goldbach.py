f0c452fefc9946fce579f37e405d7ed1ca61c4b9
Rafael Felix <rafelix@berkeley.edu>
2018-04-19 00:48:44 -0700

    Goldbach's Weak Conjecture Challenge

---
+++ goldbach.py
@@ -0,0 +1,35 @@
+import math
+
+def is_prime(num):
+	"""Computes if a number is a prime, by checking primality 
+	with all the numbers from 2 to n-1."""
+	if num < 2:
+		return False
+	else:
+		k = num - 1
+		while k > 1:
+			if num % k == 0:
+				return False
+			k -= 1
+		return True
+
+def sieve(num):
+	"""Computes all primes less than or equal to a number,
+	following the Sieve of Erathostenes."""
+	set_nums = set(range(2, num))
+	k = 2
+	while k <= math.sqrt(num):
+		delete_set = set()
+		for el in set_nums:
+			if el % k == 0 and el != k:
+				delete_set.add(el)
+		k += 1
+		set_nums = set_nums - delete_set
+	return set_nums
+
+def goldbach_weak_conjecture(num):
+	"""Returns a group of 3 primes that sum up to a number."""
+	sieve_ = sorted(sieve(num), reverse=True)
+	sumands = next([i, j] for i in sieve_ for j in sieve_ if i + j < num - 1 and is_prime(num - (i + j)))
+	results = [[num - (sumands[0] + sumands[1]), sumands[0], sumands[1]]]
+	return results[0]
